70b50291687fe2346cece7c21f4ee6175c0f8953
Cash <ca5h@qq.com>
2017-04-19 18:05:02 +0800

    index logs

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var client = require('./routes/client');
+var server = require('./routes/server');
 
 var app = express();
 
@@ -23,8 +23,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', client);
+app.use('/client', client);
+app.use('/server', server);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
