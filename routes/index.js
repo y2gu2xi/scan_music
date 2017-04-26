b06d8b499573ce242a2ff71fbbb5e32b4d074e58
Cash <ca5h@qq.com>
2017-04-26 18:38:39 +0800

    center

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Processing...' });
+  res.render('index', { title: 'O(∩_∩)O~' });
 });
 
 module.exports = router;
